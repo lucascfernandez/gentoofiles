47ff2be0c5332cf5c98ce39fd3bb7c0bcb352b8b
lucascfernandez <lucasc.fernandez@gmail.com>
2021-12-15 20:51:03 -0300

    actualiza

--- scr/yt.sh
+++ scr/yt.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-dmenu=$(echo -e "canales\nvideo\nmusica" | dmenu -i -p "輸 YouTube:")
+dmenu=$(echo -e " canales\nﳲ video\n阮 musica\n anime" | dmenu -i -p "輸 Stream:")
 
 
 case "$dmenu" in
-	"canales") ytfzf -D -S -f ;;
-	"video") alacritty -e ytfzf -tf ;;
-	"musica") ytfzf -D -m ;;
+	" canales") ytfzf -D -S -f ;;
+	"ﳲ video") alacritty -e ytfzf -tf ;;
+	"阮 musica") alacritty -e ytfzf -tm ;;
+	" anime") alacritty -e ani-cli -q 480 ;;
 
 esac
 
