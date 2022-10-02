44dd745ae2e07818fcfc7943a7ee4610b8bc87d7
lucascfernandez <lucasc.fernandez@gmail.com>
2022-10-02 18:25:50 -0300

    Simplifica configuración

--- emacs/init.el
+++ emacs/init.el
@@ -17,7 +17,7 @@
  '(org-startup-indented t)
  '(org-startup-numerated t)
  '(package-selected-packages
-   '(orderless ef-themes org-preview-html cursory tmr lin logos denote fira-code-mode no-littering vertico pdf-tools olivetti rainbow-mode modus-themes ox-reveal visual-fill-column use-package shrink-path rainbow-delimiters org-bullets goto-chg elisp-refs dired-open dired-hide-dotfiles annalist))
+   '(orderless ef-themes org-preview-html cursory tmr lin logos fira-code-mode no-littering vertico pdf-tools olivetti rainbow-mode modus-themes ox-reveal visual-fill-column use-package shrink-path rainbow-delimiters org-bullets goto-chg elisp-refs dired-open dired-hide-dotfiles annalist))
  '(pdf-annot-default-annotation-properties
    '((t
       (label . ""))
@@ -43,4 +43,26 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t (:family "Ubuntu Mono" :height 160))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "black" :font "ETBembo" :height 2.0 :underline nil))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo"))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+ '(variable-pitch ((t (:family "ETBembo" :height 180 :weight thin)))))
