97901e77d89c85d1807bd86c53dfd0eaa244dbc7
lucascfernandez <lucasc.fernandez@gmail.com>
2022-11-20 20:39:15 -0300

    commit de actualización por defecto para pequeñas modificaciones

--- init.el
+++ init.el
@@ -1,49 +1,3 @@
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(setq package-enable-at-startup nil)
-
-(straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
-
-(straight-use-package 'org)
-
-(straight-use-package
- '(nano-emacs :type git :host github :repo "rougier/nano-emacs"))
-
-(require 'nano-base-colors)
-(require 'nano-colors)
-(require 'nano-faces)
-(require 'nano-theme-dark)
-;;(require 'nano-theme-light)
-(require 'nano-theme)
-(require 'nano-splash)
-(require 'nano-modeline)
-(require 'nano-layout)
-(require 'nano-defaults)
-(require 'nano-session) ; comment to see the bug
-(require 'nano-bindings)
-
-(nano-faces)
-(nano-theme)
-
-
-;; (provide 'init)
-;; ;;; init.el ends here
-
-
-
-
 (require 'org)
 (org-babel-load-file (expand-file-name "/home/lucas/.emacs.d/config.org"))
 
@@ -54,7 +8,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-roam-completion-everywhere t))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(custom-safe-themes
+   '("bfe046af7359f81d6bd4c47e09ecba8304940107752616124fbf405208b90c8f" "c505ae23385324c21821b24c9cc1d68d8da6f3cfb117eb18826d146b8ec01b15" default))
+ '(hl-sexp-background-color "#efebe9")
+ '(package-selected-packages
+   '(vertico use-package toc-org pdf-tools org-roam-ui org-noter orderless olivetti neotree leuven-theme emojify consult)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
