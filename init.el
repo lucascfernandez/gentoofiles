8240fcd2a31191e31ce9ec9a7c41bd473e15112c
lucascfernandez <lucasc.fernandez@gmail.com>
2022-11-21 12:42:37 -0300

    commit de actualización por defecto para pequeñas modificaciones

--- init.el
+++ init.el
@@ -1,22 +1,12 @@
 (require 'org)
 (org-babel-load-file (expand-file-name "/home/lucas/.emacs.d/config.org"))
 
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-safe-themes
-   '("bfe046af7359f81d6bd4c47e09ecba8304940107752616124fbf405208b90c8f" "c505ae23385324c21821b24c9cc1d68d8da6f3cfb117eb18826d146b8ec01b15" default))
- '(hl-sexp-background-color "#efebe9")
- '(package-selected-packages
-   '(vertico use-package toc-org pdf-tools org-roam-ui org-noter orderless olivetti neotree leuven-theme emojify consult)))
+ '(org-roam-completion-everywhere t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
