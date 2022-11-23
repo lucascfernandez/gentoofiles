05f1f485e52ed328d7a1b31b674934360b4ca2b2
lucascfernandez <lucasc.fernandez@gmail.com>
2022-11-23 20:37:05 -0300

    commit de actualización por defecto para pequeñas modificaciones

--- init.el
+++ init.el
@@ -1,12 +1,14 @@
 (require 'org)
 (org-babel-load-file (expand-file-name "/home/lucas/.emacs.d/config.org"))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-roam-completion-everywhere t))
+ '(custom-safe-themes
+   '("bfe046af7359f81d6bd4c47e09ecba8304940107752616124fbf405208b90c8f" default))
+ '(package-selected-packages
+   '(leuven-theme org-roam-ui olivetti toc-org consult orderless vertico use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
