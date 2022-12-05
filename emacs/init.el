709622904ce72b45234c9406ff45d4dbe7c28562
lucascfernandez <lucasc.fernandez@gmail.com>
2022-12-05 16:33:17 -0300

    commit de actualización por defecto para pequeñas modificaciones

--- emacs/init.el
+++ emacs/init.el
@@ -9,7 +9,9 @@
    '("bfe046af7359f81d6bd4c47e09ecba8304940107752616124fbf405208b90c8f" default))
  '(org-gtd-mode t)
  '(package-selected-packages
-   '(corfu org-gtd dired-sidebar org-noter pdf-tools window marginalia org-present visual-fill-column leuven-theme org-roam-ui olivetti toc-org consult orderless vertico use-package)))
+   '(notmuch corfu org-gtd dired-sidebar org-noter pdf-tools window marginalia org-present visual-fill-column leuven-theme org-roam-ui olivetti toc-org consult orderless vertico use-package))
+ '(smtpmail-smtp-server "smtp.google.com" t)
+ '(smtpmail-smtp-service 25 t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
