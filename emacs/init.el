a1c6bb27063c0c767ca8f462ec90e195be03750c
lucascfernandez <lucasc.fernandez@gmail.com>
2022-12-06 21:53:02 -0300

    commit de actualización por defecto para pequeñas modificaciones

--- emacs/init.el
+++ emacs/init.el
@@ -7,9 +7,16 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("c505ae23385324c21821b24c9cc1d68d8da6f3cfb117eb18826d146b8ec01b15" "bfe046af7359f81d6bd4c47e09ecba8304940107752616124fbf405208b90c8f" default))
+ '(dired-sidebar-width 20)
  '(olivetti-body-width 62)
  '(org-gtd-mode t)
  '(package-selected-packages
    '(org-gtd dired-sidebar org-noter pdf-tools window org-present visual-fill-column org-roam-ui olivetti toc-org consult orderless vertico use-package))
  '(smtpmail-smtp-server "smtp.google.com" t)
  '(smtpmail-smtp-service 25 t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
